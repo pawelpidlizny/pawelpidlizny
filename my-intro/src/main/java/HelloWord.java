3af1bc0d5ec4965353dedb19c3b6f4671cc33e0d
pawelpidlizny <pawelpidluzny@gmail.com>
2021-10-06 18:25:11 +0300

    Zadanie:Hello World!

---
+++ my-intro/src/main/java/HelloWord.java
@@ -0,0 +1,8 @@
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("Hello Word");
+        System.out.println("Mam na imę Pawel");
+        System.out.println("Lat mam 37");
+        System.out.println("Pierwsza litera nazwiska 'P' ");
+    }
+}
