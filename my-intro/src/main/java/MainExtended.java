61808ed43ef9c578b2ec8fa77af78177897ea656
pawelpidlizny <pawelpidluzny@gmail.com>
2021-10-06 22:29:00 +0300

    Zadanie:Deklarowanie i wywolanie zmiennych

---
+++ my-intro/src/main/java/MainExtended.java
@@ -0,0 +1,10 @@
+public class MainExtended {
+    public static void main(String[] args) {
+
+        String myName = "Pawel";
+        int myAge = 37;
+        double height = 1.90;
+        boolean isGoodLaptop = false;
+        System.out.println(myName);
+    }
+}
