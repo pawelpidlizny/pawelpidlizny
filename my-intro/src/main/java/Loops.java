2c2eea76e9dae9d3516301f2e89fc164afd555b6
pawelpidlizny <pawelpidluzny@gmail.com>
2022-02-01 23:21:05 +0200

    Zadanie:For - While

---
+++ my-intro/src/main/java/Loops.java
@@ -0,0 +1,10 @@
+public class Loops {
+    public static void main(String[] args) {
+        int[] numbers = new int [10];
+
+        for (int i = 0; i < 10; i++) {
+            numbers[i] = i + 1;
+            System.out.println(i);
+        }
+    }
+}
