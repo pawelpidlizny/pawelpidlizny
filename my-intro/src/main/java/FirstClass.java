ce7a9569f67820ff43b9d981867ee40fa8e47092
pawelpidlizny <pawelpidluzny@gmail.com>
2022-04-25 18:13:28 +0300

    Klasy i obiekty w Javie

--- my-intro/src/main/java/FirstClass.java
+++ my-intro/src/main/java/FirstClass.java
@@ -1,5 +1,16 @@
 public class FirstClass {
     public static void main(String[] args) {
-        System.out.println("Hello from FirstClass!");
+        Notebook notebook = new Notebook("600g",1000);
+        System.out.println(notebook.weight + " - " + notebook.price);
+        notebook.checkPrice();
+
+        Notebook heavyNotebook = new Notebook("2000g",1500);
+        System.out.println(heavyNotebook.weight + " - " + heavyNotebook.price);
+        heavyNotebook.checkPrice();
+
+        Notebook oldNotebook = new Notebook("1200g",500);
+        System.out.println(oldNotebook.weight + " - " + oldNotebook.price);
+        oldNotebook.checkPrice();
+
     }
 }
