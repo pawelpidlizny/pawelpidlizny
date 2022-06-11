3c5306251ca3c606e111e87e7672b3a0951e3ec1
pawelpidlizny <pawelpidluzny@gmail.com>
2022-06-11 18:52:50 +0300

    2.8. Instrukcja warunkowa switch cwiczenie

---
+++ my-intro/src/main/java/AdvCalculator.java
@@ -0,0 +1,16 @@
+public class AdvCalculator {
+    public double calculate(){
+        String userSelected = UserDialogs.getUserSelection();
+        int a = UserDialogs.getValue();
+        int b = UserDialogs.getValue();
+        double result =0;
+        switch (userSelected) {
+            case "ADD": return a + b;
+            case "SUB": return a - b;
+            case "DIV": return a / b;
+            case "MUL": return a * b;
+            default:return 0;
+        }
+
+    }
+}
--- my-intro/src/main/java/UserDialogs.java
+++ my-intro/src/main/java/UserDialogs.java
@@ -1,22 +1,45 @@
 import java.util.Scanner;
 
 public class UserDialogs {
-    public static String getUsername(){
+    public static String getUsername() {
         Scanner scanner = new Scanner(System.in);
         //[1]
 
-        while (true){
+        while (true) {
             //[2]
             System.out.println("Wpisz swoeje imię:");
             //[3]
             String name = scanner.nextLine().trim();
             //[4]
-            if (name.length()>=2){
+            if (name.length() >= 2) {
                 //[5]
                 return name;
                 //[6]
             }
             System.out.println("Imię jest za krótkie. Spróbuj ponownie.");
         }
+    }
+    public static String getUserSelection() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Select calculation (A-add, S-subtract, D-divide, M-multiply):");
+
+            String calc = scanner.nextLine().trim().toUpperCase(); //[1]
+            switch (calc) {
+
+                case "A": return "ADD";
+                case "S": return "SUB";
+                case "D": return "DIV";
+                case "M": return "MUL";
+                default:
+                    System.out.println("Wrong calculation. Try again.");
+            }
+        }
+    }
+    public static int getValue(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number:");
+        int val = scanner.nextInt();
+        return val;
     }
 }
