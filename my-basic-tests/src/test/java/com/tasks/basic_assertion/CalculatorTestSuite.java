42d837c5ac7e990772c4d266a050e2cef16eb8ae
pawelpidlizny <pawelpidluzny@gmail.com>
2022-06-27 20:34:24 +0300

    Zadanie: testy jednostkowe dla kalkulatora

---
+++ my-basic-tests/src/test/java/com/tasks/basic_assertion/CalculatorTestSuite.java
@@ -0,0 +1,30 @@
+package com.tasks.basic_assertion;
+import com.mytasks.basic_assertion.Calculator;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTestSuite{
+    @Test
+    public void testSum(){
+        Calculator calculator = new Calculator();
+        int a = 5;
+        int b = 8;
+        int sumResult = calculator.sum(a,b);
+        Assertions.assertEquals (13,sumResult);
+    }
+    @Test
+    public void testSub(){
+        Calculator calculator = new Calculator();
+        int x = 5;
+        int y = 8;
+        int sumResult = calculator.subtract (x,y);
+        Assertions.assertEquals(-3,sumResult);
+    }
+    @Test
+    public void  testPower(){
+        Calculator calculator = new Calculator();
+        int z = 8;
+        int sumResult = calculator.power(z);
+        Assertions.assertEquals(64,sumResult);
+    }
+}
