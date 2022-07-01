2e4950bf0ef206fb7a55b0140be75f22c449138f
pawelpidlizny <pawelpidluzny@gmail.com>
2022-07-01 11:02:07 +0300

    3.7. Test Driven Development

---
+++ my-basic-tests/src/test/java/com/tasks/basic_assertion/PrimeCheckerTestSuite.java
@@ -0,0 +1,59 @@
+package com.tasks.basic_assertion;
+import com.mytasks.basic_assertion.PrimeChecker;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class PrimeCheckerTestSuite {
+    private  static int count = 0;
+
+    @Test
+    public void shouldReturnFalseWhenPassingNotPrimeNumber(){
+        PrimeChecker checker = new PrimeChecker();
+        boolean result = checker.isPrime(14);
+        count++;
+        System.out.println("Test number " + count);
+        assertFalse(result);
+    }
+    @Test
+    public void shouldReturnTrueWhenPassingPrimeNumber(){
+        PrimeChecker checker = new PrimeChecker();
+        boolean result = checker.isPrime(13);
+        checker.incrementCount();
+        System.out.println("Count:" + checker.getCount());
+        assertTrue(result);
+    }
+    @Test
+    public void shouldReturnTrueWhenPassingTwo(){
+        PrimeChecker checker = new PrimeChecker();
+        boolean result = checker.isPrime(2);
+        checker.incrementCount();
+        System.out.println("Count: "  + checker.getCount());
+        assertTrue(result);
+    }
+    @Test
+    public void shouldReturnFalseWhenPassingOne(){
+        PrimeChecker checker = new PrimeChecker();
+        boolean result = checker.isPrime(1);
+        checker.incrementCount();
+        System.out.println("Count:" + checker.getCount());
+        assertFalse(result);
+    }
+    @Test
+    public void shouldReturnFalseWhenPassingZero(){
+        PrimeChecker checker = new PrimeChecker();
+        boolean result = checker.isPrime(0);
+        checker.incrementCount();
+        System.out.println("Count:" + checker.getCount());
+        assertFalse(result);
+    }
+    @Test
+    public void shouldReturnFalseWhenPassingNegativeNumber(){
+        PrimeChecker checker = new PrimeChecker();
+        boolean result = checker.isPrime(-6);
+        checker.incrementCount();
+        System.out.println("Count:" + checker.getCount());
+        assertFalse(result);
+    }
+}
